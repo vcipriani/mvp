2a545d5190761fb57cd541b3b3d2e2e27706696c
Vince Cipriani <vince.cipriani@gmail.com>
2016-02-15 11:56:40 -0800

    add a sample API endpoint to validate that the customer site can fetch JS and render to the screen

--- server/server.js
+++ server/server.js
@@ -7,11 +7,19 @@ var port = 3000;
 //Serve our admin app
 app.use('/app', express.static(path.join(__dirname, '../client')));
 
-//Serve our example user site.  This would normally be hosted by the customer, not us
+//Serve our example customer site.  This would normally be hosted by the customer, not us
 app.use('/cust', express.static(path.join(__dirname, '../customer')));
 
 app.get('/', function(req, res) {
   res.send('hello world');
+});
+
+//A sample endpoint which would contain the logic required to render the content specified by the customer
+app.get('/content/1', function(req, res) {
+  res.send('var div = document.createElement(\'div\'); ' +
+                      'var text = document.createTextNode(\'Hello World\'); ' +
+                      'div.appendChild(text); ' +
+                      'document.body.appendChild(div);');
 });
 
 app.listen(port, function () {
